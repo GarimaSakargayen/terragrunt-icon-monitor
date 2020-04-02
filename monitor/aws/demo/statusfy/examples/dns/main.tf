51d7e988aec6b15c89f2927cf1641f8c54601861
rob <robcannonxyz@gmail.com>
2020-04-02 14:41:46 -0700

    adding statusfy init

---
+++ monitor/aws/demo/statusfy/examples/dns/main.tf
@@ -0,0 +1,16 @@
+variable "public_key" {}
+variable "private_key_path" {}
+variable "root_domain_name" {}
+variable "hostname" {}
+
+
+module "this" {
+  source = "../.."
+  private_key_path = var.private_key_path
+  public_key = var.public_key
+
+}
+
+output "public_ip" {
+  value = module.this.public_ip
+}
